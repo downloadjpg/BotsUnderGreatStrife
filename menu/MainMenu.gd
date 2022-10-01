61cf10081561b47d1cc5a016307399ad69d9093a
download.jpg <brody.foster@gmail.com>
2022-10-01 17:39:06 -0400

    bitch be fuckin for real rn

--- menu/MainMenu.gd
+++ menu/MainMenu.gd
@@ -4,4 +4,4 @@ export(String, FILE, "*.tscn") var next_scene
 
 
 func _on_Button_pressed():
-	get_tree().get_root().get_child(0).change_scene("res://world.tscn")
+	get_tree().get_root().get_child(0).change_scene(next_scene)
