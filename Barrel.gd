3ffe380bf8c3abae82e1beb3af43889f5c9887f4
download.jpg <brody.foster@gmail.com>
2022-10-02 18:41:48 -0400

    explosion sound

--- Barrel.gd
+++ Barrel.gd
@@ -7,5 +7,7 @@ func hit(damage):
 	var explosion = Explosion.instance()
 	get_tree().current_scene.current_scene.add_child(explosion)
 	explosion.global_position = global_position
+	var camera = get_tree().get_nodes_in_group("Camera")[0]
+	camera.medium_shake()
 	queue_free()
 	
